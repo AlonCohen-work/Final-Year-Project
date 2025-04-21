e1293c6a04ca38f778dc0b4d3b825365b3509aa4
sindikoi <sindikoifman@gmail.com>
2025-04-21 11:46:29 +0300

    only started the implemention on the managers

--- Python/scheduler_manager.py
+++ Python/scheduler_manager.py
@@ -15,7 +15,45 @@ def run_scheduler_manager(algo_result):
     for var_name, var_info in variables.items():
         if var_info['position'] == 'Shift Supervisor':
             manager_slot[var_name] = var_info
-    print(f'found {len(manager_slot)} manger variables')        
+    print(f'found {len(manager_slot)} manger variables')     
+
+    # קורא לפונקציה ומדפיס את התוצאה
+    solution = find_manager_that_avaliable(manager_slot, shift_managers)
+    
+    if solution:
+        for var_name, assigned_manager in solution.items():
+            print(f"filed: {var_name}")
+            print(f"manager: {assigned_manager['name']}")
+            print("---")
+    else:
+        print("No solution found!")
+
+#add variables to problem
+# עובר על כל יום ומשמרת ובודק שהיום והמשמרת שהאחראי בחר מתאימה ליום שדרוש אצל המנהל 
+def find_manager_that_avaliable(manager_slot, shift_managers):
+    problem = Problem() 
+ 
+    for var_name, var_info in manager_slot.items():
+        avaliable_managers = [] 
+        for manager in shift_managers:
+            for day_info in manager['selectedDays']:
+                if day_info['day'] == var_info['day'] and var_info['shift'] in day_info['shifts']:
+                    avaliable_managers.append(manager)
+                    break
+
+        problem.addVariable(var_name, avaliable_managers)  
+
+    solution = problem.getSolution()
+
+    return solution            
+
+
+
+
+    
+    
+
+    
 
 if __name__ == "__main__":
     manager_id = 4
